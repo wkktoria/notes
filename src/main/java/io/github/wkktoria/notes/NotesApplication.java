0e5179490779b69794e1fb3084bbfa8f81755d31
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-11-23 22:18:52 +0100

    Add validator

--- src/main/java/io/github/wkktoria/notes/NotesApplication.java
+++ src/main/java/io/github/wkktoria/notes/NotesApplication.java
@@ -2,10 +2,26 @@ package io.github.wkktoria.notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
-public class NotesApplication {
+public class NotesApplication implements RepositoryRestConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(NotesApplication.class, args);
+    }
+
+    @Override
+    public void configureValidatingRepositoryEventListener(final ValidatingRepositoryEventListener validatingListener) {
+        validatingListener.addValidator("beforeCreate", validator());
+        validatingListener.addValidator("beforeSave", validator());
+    }
+
+    @Bean
+    Validator validator() {
+        return new LocalValidatorFactoryBean();
     }
 }
